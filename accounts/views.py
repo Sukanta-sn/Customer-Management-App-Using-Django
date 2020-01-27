30abe706254d49c479a04bb5456187e640eb1533
Sukanta Nandi <sukantanandi0@gmail.com>
2020-01-27 14:58:04 +0530

    Models Created

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse('Home')
+	return render(request, 'accounts/dashboard.html')
 
 def products(request):
-	return HttpResponse('Products')
+	return render(request, 'accounts/products.html')
 
 def customers(request):
-	return HttpResponse('Customers')
+	return render(request, 'accounts/customers.html')
 
 
